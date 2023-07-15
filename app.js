3db961d3ee6d44af7c903c998cd6b34e992b57a2
Adnan1420 <ami949@uregina.ca>
2023-07-15 11:57:36 +0530

    added basic login

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
+const PORT = process.env.PORT || 3000;
 const passportLocalMongoose = require("passport-local-mongoose");
+const bodyParser = require("body-parser");
+
+const restaurantRoutes = require("./routes/restaurant");
 require("dotenv").config();
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use("/profile", express.static("upload/images"));
+// restaurant routes
+app.use("/restaurant", restaurantRoutes);
 
-app.set('view engine', 'html')
-app.use(express.static('public'))
-
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
